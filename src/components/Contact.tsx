a934e7218915f0e1b8ce4997b0ccc0ac4cf7e71e
Muhammed anzarshah <anzarsha3240@gmail.com>
2025-08-03 07:59:47 +0530

    Fix platform-specific dependency issue

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -46,11 +46,13 @@ const Contact = () => {
           setLoading(false);
           console.log("SUCCESS!");
           toast.success("Email sent");
+          form.current?.reset();
         },
         (error) => {
           setLoading(false);
           console.log("FAILED...", error.text);
           toast.warning("Failed to send");
+          form.current?.reset();
         }
       );
   };
